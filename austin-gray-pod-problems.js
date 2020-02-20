46448c850a74182b42ab8a33b124867928d59e0f
Austin Gray <Austin.m.gray210@gmail.com>
2020-02-19 19:13:12 -0600

    updated pod problems and solutions

---
+++ austin-gray-pod-problems.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// 1. Create a function named wordsLength that takes an array of words and transforms it into an array of each word's length.
+// wordLengths(["hello", "world"]) ➞ [5, 5]
+// wordLengths(["Halloween", "Thanksgiving", "Christmas"]) ➞ [9, 12, 9]
+// wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]) ➞ [3, 5, 9, 4, 2, 3, 8]
+
+
+
+// 2. https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/using-objects-for-lookups //
+
+// // Setup
+// function phoneticLookup(val) {
+//     var result = "";
+//
+//     // Only change code below this line
+//     switch(val) {
+//         case "alpha":
+//             result = "Adams";
+//             break;
+//         case "bravo":
+//             result = "Boston";
+//             break;
+//         case "charlie":
+//             result = "Chicago";
+//             break;
+//         case "delta":
+//             result = "Denver";
+//             break;
+//         case "echo":
+//             result = "Easy";
+//             break;
+//         case "foxtrot":
+//             result = "Frank";
+//     }
+//
+//     // Only change code above this line
+//     return result;
+// }
+//
+// // Change this value to test
+// phoneticLookup("charlie");
+
+
+
+
+// 3. Create a function that returns the total number of steps it takes to transform each element to the maximal element in the array. Each step consists of incrementing a digit by one.
+
+
---
+++ austin-gray-pod-solutions.js
@@ -0,0 +1,46 @@
+"use strict";
+// SOURCE:  https://edabit.com/challenge/B9yPEStmax7Jxb9Dj  //
+// Create a function that takes an array of words and transforms it into an array of each word's length.
+// wordLengths(["hello", "world"]) ➞ [5, 5]
+// wordLengths(["Halloween", "Thanksgiving", "Christmas"]) ➞ [9, 12, 9]
+// wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]) ➞ [3, 5, 9, 4, 2, 3, 8]
+
+// function wordLengths(array){
+//     var arrayLength = [];
+//     for (var i = 0; i < array.length; i++){
+//         arrayLength.push(array[i].length)
+//     }
+//     return arrayLength;
+// }
+
+
+// const wordLengths = arr => arr.map(word => word.length);
+
+
+
+console.log(wordLengths(["hello", "how", "are", "you", "doing?"]));
+// Should return [5, 3, 3, 3, 5] //
+
+
+
+
+// // Setup
+// function phoneticLookup(val) {
+//     var result = "";
+//
+//     // Only change code below this line
+//     var lookup = {
+//         "alpha": "Adams",
+//         "bravo": "Boston",
+//         "charlie": "Chicago",
+//         "delta": "Denver",
+//         "echo": "Easy",
+//         "foxtrot": "Frank",
+//     };
+//     return lookup[val];
+//     // Only change code above this line
+//     return result;
+// }
+//
+// // Change this value to test
+// phoneticLookup("charlie");
